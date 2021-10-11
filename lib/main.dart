bf380722fb8a9926e50519d70c5358ee728b5b44
FlutLab.io <>
2021-10-11 01:21:45 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -33,7 +33,7 @@ class _State extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-
+    return Scaffold(
       body: Padding(
         padding: EdgeInsets.all(10),
         child: Column(
